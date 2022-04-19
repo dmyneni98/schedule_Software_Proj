24f6c4c008be73e9edee81b67317286e0bf8e921
R_Olarry <tomiwaolarewaju@R-Olarrys-MacBook-Pro.local>
2022-04-19 18:19:49 -0500

    setting up django

---
+++ project5.cpp
@@ -0,0 +1,18 @@
+//Collatz Correction mainly 
+
+static_global_ void collatz(const long top, int* const maxlen):
+    const long i = 3 + (all the thread stuff) * 2;
+
+
+//Executing timesd code 
+collatz <<< ((top -2 )/ 2 + ThreadsPerBlock -1)/ ThreadsPerBlock, ThreadsPerBlock>>>(top,d_maxlen);
+
+
+
+
+//K-means CUDA correction 
+
+const i = (normal thread stuff);
+
+
+
